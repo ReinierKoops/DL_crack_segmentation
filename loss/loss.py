ef0e628b2d6f0f967720153c092a8bf653983959
Reinier Koops <info@reinier.work>
2021-04-16 11:26:27 +0200

    Refactored code

--- loss/loss.py
+++ loss/loss.py
@@ -23,16 +23,3 @@ def batch_dice_loss(true_val, pred_val, epsilon=1e-8):
     denominator = (pred_val).sum(dim=1) + (true_val).sum(dim=1)
 
     return torch.mean(1 - ((numerator + epsilon) / (denominator + epsilon)))
-
-#
-# prediction = torch.randint(low=-255, high=256, size=(3, 3), dtype=torch.float)
-
-# pred_is_truth = (prediction >= 0).float()
-# pred_is_false = (prediction < 0).float()
-# pred_is_half = torch.flip(pred_is_truth, [0, 1])
-
-# print(pred_is_truth, "\n\n", pred_is_false, "\n\n", pred_is_half)
-
-# print(f"different tensors should output 1: {dice_loss(pred_is_false, prediction)}")
-# print(f"the opposite tensors should output 0: {dice_loss(pred_is_truth, prediction)}")
-# print(f"the avg tensors should output [0,1]: {dice_loss(pred_is_half, prediction)}")
